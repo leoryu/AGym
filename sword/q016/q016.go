a00fcd5674fa5caa51fdaa9d00739f83b3f4f735
leoryu <leo-ryu@outlook.com>
2019-08-07 16:10:32 +0800

    add q016

---
+++ sword/q016/q016.go
@@ -0,0 +1,20 @@
+package q015
+
+type NodeList struct {
+	Value    int
+	NextNode *NodeList
+}
+
+func Answer(head *NodeList) (target *NodeList) {
+	if head == nil || head.NextNode == nil {
+		return head
+	}
+	var tmpNode *NodeList
+	for head != nil {
+		next := head.NextNode
+		head.NextNode = tmpNode
+		tmpNode = head
+		head = next
+	}
+	return tmpNode
+}
---
+++ sword/q016/q016_test.go
@@ -0,0 +1,65 @@
+package q015
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAnswer(t *testing.T) {
+	type args struct {
+		head *NodeList
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantTarget *NodeList
+	}{
+		{
+			"Function test 1",
+			args{
+				head: &NodeList{
+					Value: 1,
+					NextNode: &NodeList{
+						Value: 2,
+						NextNode: &NodeList{
+							Value:    3,
+							NextNode: nil,
+						},
+					},
+				},
+			},
+			&NodeList{
+				Value: 3,
+				NextNode: &NodeList{
+					Value: 2,
+					NextNode: &NodeList{
+						Value:    1,
+						NextNode: nil,
+					},
+				},
+			},
+		},
+		{
+			"Zero test",
+			args{
+				head: nil,
+			},
+			nil,
+		},
+		{
+			"Test for single node",
+			args{
+				head: &NodeList{},
+			},
+			&NodeList{},
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotTarget := Answer(tt.args.head); !reflect.DeepEqual(gotTarget, tt.wantTarget) {
+				t.Errorf("Answer() = %v, want %v", gotTarget, tt.wantTarget)
+			}
+		})
+	}
+}
